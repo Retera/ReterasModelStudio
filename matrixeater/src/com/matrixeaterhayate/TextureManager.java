82c66e7edf3f383257e62032ac04a8cb316c6c85
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2026-01-11 04:25:54 +0800

    Update TextureManager.java

--- matrixeater/src/com/matrixeaterhayate/TextureManager.java
+++ matrixeater/src/com/matrixeaterhayate/TextureManager.java
@@ -23,6 +23,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileFilter;
 
+import com.matrixeater.localization.LocalizationManager;
 import com.hiveworkshop.wc3.gui.BLPHandler;
 import com.hiveworkshop.wc3.gui.datachooser.DataSource;
 import com.hiveworkshop.wc3.gui.modeledit.actions.newsys.ModelStructureChangeListener;
@@ -258,3 +259,4 @@ public class TextureManager extends JPanel {
 		}
 	}
 }
+
