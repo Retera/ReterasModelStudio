f5664747ddd9b0768b8c9f1b2cae683ef0114aa6
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2026-01-11 05:29:09 +0800

    Update YseraPanel.java

--- matrixeater/src/ysera/YseraPanel.java
+++ matrixeater/src/ysera/YseraPanel.java
@@ -62,19 +62,12 @@ public class YseraPanel extends JPanel {
 		jMenuBar.add(new JMenu(lm.get("menu.team_color")));
 		jMenuBar.add(new JMenu(lm.get("menu.windows")));
 		jMenuBar.add(new JMenu(lm.get("menu.extras")));
-		//jMenuBar.add(new JMenu(lm.get("menu.help")));
-		jMenuBar.add(new JMenu(lm.get("menu.language")));
-
-		final JMenuItem enItem = new JMenuItem(lm.get("language.en"));
-		enItem.addActionListener(e -> LocalizationManager.getInstance().setLocale(Locale.ENGLISH));
-		final JMenuItem zhItem = new JMenuItem(lm.get("language.zh_CN"));
-		zhItem.addActionListener(e -> LocalizationManager.getInstance().setLocale(new Locale("zh","CN")));
-		new JMenu(lm.get("menu.language")).add(enItem);
-		new JMenu(lm.get("menu.language")).add(zhItem);
+		jMenuBar.add(new JMenu(lm.get("menu.help")));
 
 		return jMenuBar;
 	}
 }
+
 
 
 
